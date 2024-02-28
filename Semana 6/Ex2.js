011f0f6b305cb5f3a02dbee0436aa52ee24cf7b4
Jorge_H <jorgeluishm88@gmail.com>
2024-02-28 19:20:06 -0300

    ex2 e ex3

--- Semana 6/Ex2.js
+++ Semana 6/Ex2.js
@@ -2,21 +2,29 @@ const BASE_URL = `http://localhost:9000/users`;
 
 const getUserInfo = async function () {
   try {
-    const res = await fetch(`${BASE_URL}`);
+    const res = await fetch(BASE_URL);
     const data = await res.json();
     return data;
-  } catch {
-    console.error(`Error loading data`);
+  } catch (error) {
+    console.error(`Error loading data: ${error.message}`);
   }
 };
 
-const showInfor = (async function () {
+const showHtml = document.getElementById("nome");
+
+const updateUserInfo = (async function () {
   try {
     await setTimeout(async () => {
-      const userInfo = await getUserInfo();
-      console.log(userInfo);
+      const users = await getUserInfo();
+
+      showHtml.innerHTML = ` 
+      Nome: ${users[0].nome}<br>
+      Idade: ${users[0].idade}<br>
+      Email: ${users[0].email}
+      `;
+      console.log(users);
     }, 2000);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error updating user info: ${error.message}`);
   }
 })();
