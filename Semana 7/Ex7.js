e8bd3cdd392956ee796e9ae970d5a95cd716714a
Jorge_H <jorgeluishm88@gmail.com>
2024-03-12 16:13:01 -0300

    Ex7-8

--- Semana 7/Ex7.js
+++ Semana 7/Ex7.js
@@ -9,3 +9,23 @@ class Pessoa {
     this.profissao = profissao;
   }
 }
+
+class Cliente extends Pessoa {
+  constructor(nome, idade, profissao, telefone, email, clienteDesde) {
+    super(nome, idade, profissao);
+    this.telefone = telefone;
+    this.email = email;
+    if (Cliente.validarFormatoData(clienteDesde)) {
+      this.clienteDesde = clienteDesde;
+    } else {
+      throw new Error(
+        "O formato da data do clienteDesde deve ser 'ANO-MES-DIA'"
+      );
+    }
+  }
+
+  static validarFormatoData(data) {
+    const regex = /^\d{4}-\d{2}-\d{2}$/;
+    return regex.test(data);
+  }
+}
